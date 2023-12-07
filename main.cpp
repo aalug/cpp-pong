ae2d066363d2ca4b37900f8bc33d20a4061f94ec
Adam Gulczyński <a.a.gulczynski@gmail.com>
2023-12-08 00:04:16 +0100

    window setup

--- main.cpp
+++ main.cpp
@@ -1,5 +1,22 @@
+#include <raylib.h>
 
 int main() {
+
+    const int screen_width = 1280;
+    const int screen_height = 800;
+    const char *title = "Pong";
+    const int fps = 60;
+
+    InitWindow(screen_width, screen_height, title);
+    SetTargetFPS(fps);
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+
+        EndDrawing();
+    }
+
+    CloseWindow();
 
     return 0;
 }
