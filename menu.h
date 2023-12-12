642b5c18407e728dfc4b1bd72a3a8de6c56deaba
Adam Gulczyński <a.a.gulczynski@gmail.com>
2023-12-12 18:38:24 +0100

    improve menu navigation display and update documentation

--- menu.h
+++ menu.h
@@ -15,6 +15,15 @@ public:
     Menu(int screen_width, int screen_height, int header_font_size, int subtitle_font_size, Color color,
          Color background_color);
 
+    /**
+     * Helper function for displaying general menu.
+     * Used by other display_menu type functions for
+     * improving code quality.
+     * @param menu_title
+     * @param options
+     * @param show_menu
+     * @param selector_position
+     */
     void display_menu(const std::string &menu_title, const std::vector<std::string> &options, bool &show_menu,
                       int &selector_position) const;
 
@@ -37,6 +46,14 @@ public:
     */
     void display_choose_difficulty_menu(bool &show_menu, int &selector_position) const;
 
+    /**
+    * Draw a menu that will allow to choose color theme.
+    * @param show_menu reference to a value based on
+    * which menu will or will not be displayed.
+    * @param selector_position reference to an int that
+    * represents color themes outside of the function,
+    * and inside a position of graphical selector.
+    */
     void display_choose_color_theme_menu(bool &show_menu, int &selector_position) const;
 };
 
